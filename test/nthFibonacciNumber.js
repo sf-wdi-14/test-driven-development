7d698a9cc6ab90e7bda43bdee7f45023daf82e8c
Dennis Charles Hackethal <dennis@generalassemb.ly>
2014-11-23 15:46:41 -0800

    Update nthFibonacciNumber.js

--- test/nthFibonacciNumber.js
+++ test/nthFibonacciNumber.js
@@ -1,1 +1,4 @@
 var expect = require('chai').expect;
+
+// Write a test for a method that takes a number n and returns the nth number of the Fibonacci sequence.
+// Then write the test that will make that number pass.
