d18a57f053986814908c06eee6c373d894e8d28b
Dennis Charles Hackethal <dennis@generalassemb.ly>
2014-11-23 15:43:42 -0800

    Update mergeArrays.js

--- test/mergeArrays.js
+++ test/mergeArrays.js
@@ -1,1 +1,4 @@
 var expect = require('chai').expect;
+
+// Write a test for a method that takes two arrays and merges them, returning one merged array.
+// Then write the method that makes that test pass.
