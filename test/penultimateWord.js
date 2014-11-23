de3bc02991c1c8545a3c4c966753b5fa92c91155
Dennis Charles Hackethal <dennis@generalassemb.ly>
2014-11-23 15:47:12 -0800

    Update penultimateWord.js

--- test/penultimateWord.js
+++ test/penultimateWord.js
@@ -1,1 +1,4 @@
 var expect = require('chai').expect;
+
+// Write a test for a method that takes a string and return the second to last word of that string.
+// Then write the method that will make that test pass.
