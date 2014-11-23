ef74eff8b5ac2dc862cbbf7f47370d45327f9730
Dennis Charles Hackethal <dennis@generalassemb.ly>
2014-11-23 15:45:52 -0800

    Update modulo.js

--- test/modulo.js
+++ test/modulo.js
@@ -1,1 +1,5 @@
 var expect = require('chai').expect;
+
+// Write a test for a method that takes two numbers, divides the first by the second number
+// and returns the modulo of the result. Do not use % (JS's internal modulo).
+// Then write the method that makes that test pass.
