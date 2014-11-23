bfa4706ec509e402dc83bb877428999817b5d7ea
Dennis Charles Hackethal <dennis@generalassemb.ly>
2014-11-23 15:49:07 -0800

    Update sumOfDigits.js

--- test/sumOfDigits.js
+++ test/sumOfDigits.js
@@ -1,1 +1,6 @@
 var expect = require('chai').expect;
+
+// Write a test for a method that takes a number and then returns the sum of all the number's digits.
+// Also test for the ability to pass in either a number as an integer, or as a string (e.g., 123 and '123'
+// should both work).
+// Then write the method that will make that test pass.
