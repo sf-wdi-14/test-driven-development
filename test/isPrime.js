22fd2ef05bd4f6be726842c32ff29b22cdf32249
Art Tucker <art.w.tucker@gmail.com>
2014-11-24 11:18:53 -0800

    Write solutions for testing

--- test/isPrime.js
+++ test/isPrime.js
@@ -4,10 +4,40 @@ describe('#isPrime()', function() {
 	it('returns true for a prime number', function() {
 		expect(isPrime(5)).to.equal(true)
 	});
+	it("returns true for a large prime number", function(){
+		expect(isPrime(961748941)).to.equal(true)
+	});
 
 	it('returns false for non-prime numbers', function() {
 		expect(isPrime(4)).to.equal(false)
 	});
+
+	it("returns false for 1", function(){
+		expect(isPrime(1)).to.equal(false)
+	});
 });
 
 // Write the method that makes this test pass.
+var isPrime = function(num){
+	if (isNaN(num) || !isFinite(num) || num%1 || num<2 || num===1) return false;
+	else if (num%2===0) {
+		console.log(2);
+		return false;
+	} else if (num%3===0) {
+		console.log(3);
+		return false;
+	} else if (num>5 && num%5===0) {
+		console.log(5);
+		return false;
+	} else {
+		var square = Math.sqrt(num);
+		for (var i = 5; i < square; i++) {
+			if (num%i===0) {
+				console.log(i);
+				return false;
+			};
+		};
+	}
+	console.log(num+" is prime!");
+	return true;
+};
