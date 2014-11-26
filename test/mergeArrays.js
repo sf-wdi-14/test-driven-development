659d302d9f0f951f6ba11c8d67c2e0161d495351
Margaret Lee <Margaret.Mi.Lee@gmail.com>
2014-11-25 16:22:54 -0800

    Update of tests

--- test/mergeArrays.js
+++ test/mergeArrays.js
@@ -2,3 +2,10 @@ var expect = require('chai').expect;
 
 // Write a test for a method that takes two arrays and merges them, returning one merged array.
 // Then write the method that makes that test pass.
+
+
+
+[1,3,5,20,100]
+[2,4,6,9,25]
+//Compare the 2 numbers are the same index
+[1,]
