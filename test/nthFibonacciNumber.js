3a7d43f3c3972cf9da4989e88e0bb98305b0b071
Protosac <proto@sacquorum.com>
2014-11-26 12:55:40 -0800

    Add fibonaccia to hudson-kay branch

--- test/nthFibonacciNumber.js
+++ test/nthFibonacciNumber.js
@@ -2,3 +2,20 @@ var expect = require('chai').expect;
 
 // Write a test for a method that takes a number n and returns the nth number of the Fibonacci sequence.
 // Then write the test that will make that number pass.
+describe('#fibonacci()', function() {
+	it('takes a number n and returns the nth number of the sequence', function() {
+		expect(fibonacci(3)).to.equal(1);
+	});
+});
+
+var fibonacci = function(f) {
+    var sum;
+    var numbers = [0,1];
+    
+    for (var i = 1; i < f; i++) {
+        sum = numbers[i] + numbers[i-1];
+        
+        numbers.push(sum);
+    }
+    return numbers[f-1];
+};
